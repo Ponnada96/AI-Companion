83c53fbb4b0019dfe4e8dae6314bd57c08dcfe1a
PrasadPonnada <PrasadPonnada09@gmail.com>
2024-02-05 16:16:50 +0530

    Added ShadCn, Clerk Authentication

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
+const RootPage = () => {
+    return (
+        <div>
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    );
+
+}
+
+export default RootPage;
