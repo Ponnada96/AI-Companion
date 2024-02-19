d95bf007d853d5e6e2cb0bed73b37044b9042d7a
PrasadPonnada <PrasadPonnada09@gmail.com>
2024-02-19 11:49:34 +0530

    EndDate issue fixes

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -56,7 +56,7 @@ export async function POST(req: Request) {
             data:{
                 stripePriceId: subscription.items.data[0].price.id,
                 stripeCurrentPeriodEnd:new Date(
-                    subscription.current_period_end * 100
+                    subscription.current_period_end * 1000
                 )
             }
          })
